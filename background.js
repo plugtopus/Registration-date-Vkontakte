c6b9659372f4dfc9cf5da318d1c312efbc81995d
Your Name <you@example.com>
2018-08-09 11:07:31 +0300

    'init3'

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.runtime.onInstalled.addListener(function(details) {
+    if (details.reason == "install" && !localStorage.landing && !localStorage['first_date_installation_ntpromo']) {
+        localStorage['first_date_installation_ntpromo'] = new Date().getTime();
+
+        chrome.management.getSelf(function(info) {
+            var ext_name = encodeURIComponent(info.name);
+            chrome.tabs.create({
+                url: 'https://sites.google.com/view/promo-extensions-welcome/?id=promo_ext_w&ext=' + ext_name
+            });
+        });
+    }
+});
